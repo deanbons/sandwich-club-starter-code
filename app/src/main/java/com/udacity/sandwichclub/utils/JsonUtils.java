7b6c26832a4bc792912a67b8b4921e7959b22185
dev <dev@devs-Air.lan>
2018-08-08 14:17:45 -0400

    Replace hard-coded string values with constant string values

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -11,16 +11,24 @@ import java.util.List;
 
 public class JsonUtils {
 
+    public static final String KEY_NAME = "name";
+    public static final String KEY_MAIN_NAME = "mainName";
+    public static final String KEY_ALSO_KNOWN_AS = "alsoKnownAs";
+    public static final String KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_INGREDIENTS = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
        Sandwich sandwich = new Sandwich();
         try {
             JSONObject sandwichDetails = new JSONObject(json);
 
-            JSONObject nameDetails = sandwichDetails.getJSONObject("name");
-            sandwich.setMainName(nameDetails.getString("mainName"));
+            JSONObject nameDetails = sandwichDetails.getJSONObject(KEY_NAME);
+            sandwich.setMainName(nameDetails.getString(KEY_MAIN_NAME));
 
             List<String> akaList = new ArrayList<String>();
-            JSONArray akaArray = nameDetails.getJSONArray("alsoKnownAs");
+            JSONArray akaArray = nameDetails.getJSONArray(KEY_ALSO_KNOWN_AS);
             if(akaArray != null) {
                 for (int i = 0; i < akaArray.length(); ++i) {
                     if (!akaArray.isNull(i)) {
@@ -30,14 +38,14 @@ public class JsonUtils {
             }
             sandwich.setAlsoKnownAs(akaList);
 
-            sandwich.setPlaceOfOrigin(sandwichDetails.getString("placeOfOrigin"));
+            sandwich.setPlaceOfOrigin(sandwichDetails.getString(KEY_PLACE_OF_ORIGIN));
 
-            sandwich.setDescription(sandwichDetails.getString("description"));
+            sandwich.setDescription(sandwichDetails.getString(KEY_DESCRIPTION));
 
-            sandwich.setImage(sandwichDetails.getString("image"));
+            sandwich.setImage(sandwichDetails.getString(KEY_IMAGE));
 
             List<String> ingrList = new ArrayList<String>();
-            JSONArray ingrArray = sandwichDetails.getJSONArray("ingredients");
+            JSONArray ingrArray = sandwichDetails.getJSONArray(KEY_INGREDIENTS);
             if(ingrArray != null) {
                 for (int i = 0; i < ingrArray.length(); ++i) {
                     if (!ingrArray.isNull(i)) {
